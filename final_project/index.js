779df8cedc196423502f4622d5d297205f2a839d
davidstasik <stasikdavid1@gmail.com>
2024-09-05 18:28:19 +0200

    Updating the code for the authentication mechanism

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,46 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  //Write the authenication mechanism here
+
+  // Check if session token exists
+  if (req.session.authorization) {
+    let token = req.session.authorization["accessToken"];
+
+    // Verify token
+    jwt.verify(token, "thisisasecret", (err, user) => {
+      if (!err) {
+        req.user = user;
+        next();
+      } else {
+        res.status(403).json({ message: "User not authenticated." });
+      }
+    });
+  } else {
+    res.status(403).json({ message: "User not logged in." });
+  }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));

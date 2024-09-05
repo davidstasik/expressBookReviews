b441792e78bd92568f0de9efe6386d2ccef9c3e8
davidstasik <stasikdavid1@gmail.com>
2024-09-05 20:31:45 +0200

    Task 5 – Completing code for getting book reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -70,8 +70,15 @@ public_users.get("/title/:title", function (req, res) {
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let isbn = req.params.isbn;
+
+  let book = books[isbn];
+
+  if (book) {
+    res.status(200).send(book.reviews);
+  } else {
+    res.status(404).json({ message: `No book with the ISBN "${isbn}" found.` });
+  }
 });
 
 module.exports.general = public_users;
